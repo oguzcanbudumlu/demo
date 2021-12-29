a3618e0831d1a6bab5791c16501390ebe86e1a25
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-12-29 22:10:20 +0300

    4343244545

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("12222");
+        System.out.println("1222244");
         SpringApplication.run(DemoApplication.class, args);
     }
 
