ab415f74218447b7e38ee7318e04625b4fe5fdf9
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-12-30 08:42:12 +0300

    fixup! testghgh

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
 
-
+        System.out.println("test");
 
         SpringApplication.run(DemoApplication.class, args);
     }
